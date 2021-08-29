3e1bfaf918bb70059b071e7bd065fcd361228cdf
Li Kai <471633877@qq.com>
2021-08-29 19:19:17 +0800

    update

---
+++ FrameworkGenerator/LookinServer/LookinServer.h
@@ -0,0 +1,18 @@
+//
+//  LookinServer.h
+//  LookinServer
+//
+//  Created by 李凯 on 2021/8/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LookinServer.
+FOUNDATION_EXPORT double LookinServerVersionNumber;
+
+//! Project version string for LookinServer.
+FOUNDATION_EXPORT const unsigned char LookinServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LookinServer/PublicHeader.h>
+
+
