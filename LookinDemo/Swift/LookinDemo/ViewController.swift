9f8c43125be7f411569b987191c8ec4258ff3f59
likai.123 <471633877@qq.com>
2022-10-10 01:07:29 +0800

    demo project

---
+++ LookinDemo/Swift/LookinDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LookinDemo
+//
+//  Created by 李凯 on 2022/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let myButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(myButton)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
