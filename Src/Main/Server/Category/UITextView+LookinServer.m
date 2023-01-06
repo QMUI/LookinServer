b969f0c859ecffd583db4732354ee59fd672d2d4
andysheng <andysheng@live.com>
2023-01-06 22:41:00 +0800

    add: trace UITextView.inputview/inputAccessoryView's window

--- Src/Main/Server/Category/UITextView+LookinServer.m
+++ Src/Main/Server/Category/UITextView+LookinServer.m
@@ -8,9 +8,24 @@
 //  https://lookin.work
 //
 
+#import <objc/runtime.h>
 #import "UITextView+LookinServer.h"
+#import "LKS_WindowDiscovery.h"
 
 @implementation UITextView (LookinServer)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Method oriMethod = class_getInstanceMethod([self class], @selector(setInputView:));
+        Method newMethod = class_getInstanceMethod([self class], @selector(lks_setInputView:));
+        method_exchangeImplementations(oriMethod, newMethod);
+        
+        oriMethod = class_getInstanceMethod([self class], @selector(setInputAccessoryView:));
+        newMethod = class_getInstanceMethod([self class], @selector(lks_setInputAccessoryView:));
+        method_exchangeImplementations(oriMethod, newMethod);
+    });
+}
 
 - (CGFloat)lks_fontSize {
     return self.font.pointSize;
@@ -22,6 +37,16 @@
 
 - (NSString *)lks_fontName {
     return self.font.fontName;
+}
+
+- (void)lks_setInputView:(UIView *)inputView {
+    [self lks_setInputView:inputView];
+    [[LKS_WindowDiscovery sharedInstance] addCustomWindowWithView:inputView];
+}
+
+- (void)lks_setInputAccessoryView:(UIView *)inputAccessoryView {
+    [self lks_setInputAccessoryView:inputAccessoryView];
+    [[LKS_WindowDiscovery sharedInstance] addCustomWindowWithView:inputAccessoryView];
 }
 
 @end
