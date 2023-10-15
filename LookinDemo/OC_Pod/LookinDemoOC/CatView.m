e9a60eed13e716a799c44adff2f6771480bf12b9
likai.123 <471633877@qq.com>
2023-10-16 00:43:44 +0800

    custom infos

--- LookinDemo/OC_Pod/LookinDemoOC/CatView.m
+++ LookinDemo/OC_Pod/LookinDemoOC/CatView.m
@@ -12,10 +12,98 @@
 
 - (NSDictionary<NSString *, id> *)lookin_customDebugInfos {
     NSDictionary<NSString *, id> *ret = @{
+        // Optional. You can set object for this key to display your custom debug infos in the right panel of Lookin.
+        // 可选。你可以配置该字段从而在 Lookin 的右侧面板展示你的自定义调试信息。
         @"properties": [self makeCustomProperties],
+        
+        // Optional. You can set object for this key to display your custom subviews in the left panel of Lookin.
+        // 可选。你可以配置该字段从而在 Lookin 的左侧面板展示你的自定义 subview 对象。
         @"subviews": [self makeCustomSubviews]
     };
     return ret;
+}
+
+- (NSArray *)makeCustomProperties {
+    NSMutableArray *properties = [NSMutableArray array];
+    
+    // string property
+    [properties addObject:@{
+        @"section": @"CatInfo",
+        @"title": @"Nickname",
+        @"value": @"British shorthair",
+        @"valueType": @"string",
+        // Optional. Set this key to allow user to modify the property value in Lookin.
+        @"setter": ^(id newValue){
+            if (![newValue isKindOfClass:[NSString class]]) {
+                return;
+            }
+            NSLog(@"Get new value from lookin: %@", newValue);
+        }
+    }];
+    
+    // number property
+    [properties addObject:@{
+        @"section": @"CatInfo",
+        @"title": @"Age",
+        @"value": @13.2,
+        @"valueType": @"number",
+        // Optional. Set this key to allow user to modify the property value in Lookin.
+        @"setter": ^(id newValue){
+            if (![newValue isKindOfClass:[NSNumber class]]) {
+                return;
+            }
+            NSLog(@"Get new value from lookin: %@", newValue);
+        }
+    }];
+    
+    // bool property
+    [properties addObject:@{
+        @"section": @"CatInfo",
+        @"title": @"IsFriendly",
+        @"value": @YES,
+        @"valueType": @"bool",
+        // Optional. Set this key to allow user to modify the property value in Lookin.
+        @"setter": ^(id newValue){
+            if (![newValue isKindOfClass:[NSNumber class]]) {
+                return;
+            }
+            NSLog(@"Get new value from lookin: %@", newValue);
+        }
+    }];
+    
+    // color property
+    [properties addObject:@{
+        @"section": @"CatInfo",
+        @"title": @"SkinColor",
+        @"value": [UIColor redColor],
+        @"valueType": @"color",
+        // Optional. Set this key to allow user to modify the property value in Lookin.
+        @"setter": ^(id newValue){
+            if (![newValue isKindOfClass:[UIColor class]]) {
+                return;
+            }
+            NSLog(@"Get new value from lookin: %@", newValue);
+        }
+    }];
+    
+    // enum property
+    [properties addObject:@{
+        @"section": @"CatInfo",
+        @"title": @"Gender",
+        @"value": @"Male",
+        @"valueType": @"enum",
+        // Set object for this key when the valueType is "enum".
+        @"allEnumCases": @[@"Male", @"Female"],
+        // Optional. Set this key to allow user to modify the property value in Lookin.
+        @"setter": ^(id newValue){
+            if (![newValue isKindOfClass:[NSString class]]) {
+                return;
+            }
+            NSLog(@"Get new value from lookin: %@", newValue);
+        }
+    }];
+    
+    return [properties copy];;
 }
 
 - (NSArray *)makeCustomSubviews {
@@ -29,7 +117,7 @@
         // Optional. If you set object for this key, there will be a wireframe in Lookin middle area. The rect is in UIWindow coordinate (not superview coordinate).
         // 可选。如果包含该 key，则 Lookin 中间会为该 subview 展示一个线框。这里的 Rect 是相对于当前 Window 的（不是相对于父元素）
         @"frameInWindow": [NSValue valueWithCGRect:CGRectMake(100, 100, 200, 300)],
-        // Optional. These infos will be displayed in the right panel of Lookin.
+        // Optional. Your custom debug infos in the right panel of Lookin.
         // 可选。这些信息会展示在 Lookin 的右侧面板。
         @"properties": @[
             @{
@@ -50,7 +138,7 @@
     [subviews addObject:@{
         @"title": @"CatBaby1",
         // Optional. You can add custom subviews recursively.
-        // 可选。你可以递归地、无限地添加你的虚拟 subview。
+        // 可选。你可以递归地添加你的虚拟 subview。
         @"subviews": @[
             @{ @"title": @"CatBabyBaby" },
             @{ @"title": @"CatBabyBaby" }
@@ -59,89 +147,5 @@
     
     return [subviews copy];
 }
-
-- (NSArray *)makeCustomProperties {
-    NSMutableArray *properties = [NSMutableArray array];
-    
-    // string
-    [properties addObject:@{
-        @"section": @"CatInfo",
-        @"title": @"Nickname",
-        @"value": @"British shorthair",
-        @"valueType": @"string",
-        // Optional. Set this key to allow user to modify the property value in Lookin.
-        @"setter": ^(id newValue){
-            if (![newValue isKindOfClass:[NSString class]]) {
-                return;
-            }
-            NSLog(@"Get new value from lookin: %@", newValue);
-        }
-    }];
-    
-    // number
-    [properties addObject:@{
-        @"section": @"CatInfo",
-        @"title": @"Age",
-        @"value": @13.2,
-        @"valueType": @"number",
-        // Optional. Set this key to allow user to modify the property value in Lookin.
-        @"setter": ^(id newValue){
-            if (![newValue isKindOfClass:[NSNumber class]]) {
-                return;
-            }
-            NSLog(@"Get new value from lookin: %@", newValue);
-        }
-    }];
-    
-    // bool
-    [properties addObject:@{
-        @"section": @"CatInfo",
-        @"title": @"IsFriendly",
-        @"value": @YES,
-        @"valueType": @"bool",
-        // Optional. Set this key to allow user to modify the property value in Lookin.
-        @"setter": ^(id newValue){
-            if (![newValue isKindOfClass:[NSNumber class]]) {
-                return;
-            }
-            NSLog(@"Get new value from lookin: %@", newValue);
-        }
-    }];
-    
-    // color
-    [properties addObject:@{
-        @"section": @"CatInfo",
-        @"title": @"SkinColor",
-        @"value": [UIColor redColor],
-        @"valueType": @"color",
-        // Optional. Set this key to allow user to modify the property value in Lookin.
-        @"setter": ^(id newValue){
-            if (![newValue isKindOfClass:[UIColor class]]) {
-                return;
-            }
-            NSLog(@"Get new value from lookin: %@", newValue);
-        }
-    }];
-    
-    // enum
-    [properties addObject:@{
-        @"section": @"CatInfo",
-        @"title": @"Gender",
-        @"value": @"Male",
-        @"valueType": @"enum",
-        // Set object for this key when the valueType is "enum".
-        @"allEnumCases": @[@"Male", @"Female"],
-        // Optional. Set this key to allow user to modify the property value in Lookin.
-        @"setter": ^(id newValue){
-            if (![newValue isKindOfClass:[NSString class]]) {
-                return;
-            }
-            NSLog(@"Get new value from lookin: %@", newValue);
-        }
-    }];
-    
-    return [properties copy];;
-}
-
 
 @end
