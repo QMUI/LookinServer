0727094b5b350d7ef9f88da3d46cca679e5daad9
likai.123 <471633877@qq.com>
2023-12-21 17:36:42 +0800

    update demo

--- LookinDemo/LookinCustomInfoDemo/LookinCustomInfoDemo/Demo/ObjectiveC/CatView.m
+++ LookinDemo/LookinCustomInfoDemo/LookinCustomInfoDemo/Demo/ObjectiveC/CatView.m
@@ -52,30 +52,8 @@
 - (NSArray *)catView_makeCustomProperties {
     NSMutableArray *properties = [NSMutableArray array];
     
-    // string property
-    [properties addObject:@{
-        // 可选项。 在 Lookin 中展示的属性组的名称。
-        // Optional. The name of the property group displayed in Lookin.
-        @"section": @"CatInfo",
-        // 必填项。在 Lookin 中展示的属性的名称。
-        // Required. The name of the property displayed in Lookin.
-        @"title": @"Nickname",
-        // 可选项。在 Lookin 中展示的属性的值。如果属性值为 nil 则不要设置该项，否则 NSDictionary 可能由于插入 nil 而 Crash。
-        // Optional. The value of the property displayed in Lookin. If the property value is nil, do not set this item, otherwise NSDictionary may crash due to inserting nil.
-        @"value": @"Sushi",
-        // 必填项。告知 Lookin 以 String 格式解析和展示该属性。
-        // Required. Specify the format in which Lookin should parse and display the property.
-        @"valueType": @"string",
-        // 可选项。如果配置了该字段，则用户可以在 Lookin 中实时修改该属性。
-        //【警告】这个 block 会被 Lookin 始终持有，因此请万分注意内存管理。
-        // Optional. If this field is configured, users can modify the property by Lookin.
-        // [Warning] This block will be retained by Lookin indefinitely, so please be extremely careful with memory management.
-        @"retainedSetter": ^(NSString *newString) {
-            NSLog(@"Try to modify by Lookin.");
-        }
-    }];
-    
-    // number property
+    // 更多类型示例参加 DogLayer.m
+    // See DogLayer.m for more examples
     [properties addObject:@{
         @"section": @"CatInfo",
         @"title": @"Age",
@@ -84,109 +62,6 @@
         @"retainedSetter": ^(NSNumber *newNumber) {
             NSLog(@"Try to modify by Lookin.");
         }
-    }];
-    
-    // bool property
-    [properties addObject:@{
-        @"section": @"Animal Info",
-        @"title": @"IsFriendly",
-        @"value": [NSNumber numberWithBool:YES],
-        @"valueType": @"bool",
-        @"retainedSetter": ^(BOOL newBool) {
-            NSLog(@"Try to modify by Lookin.");
-        }
-    }];
-    
-    // color property
-    [properties addObject:@{
-        @"section": @"Animal Info",
-        @"title": @"SkinColor",
-        @"value": [UIColor blueColor],
-        @"valueType": @"color",
-        @"retainedSetter": ^(UIColor *newColor) {
-            NSLog(@"Try to modify by Lookin.");
-        }
-    }];
-    
-    // rect property
-    [properties addObject:@{
-        @"section": @"Animal Info",
-        @"title": @"CatRect",
-        @"value": [NSValue valueWithCGRect:CGRectMake(1, 2, 3, 4)],
-        @"valueType": @"rect",
-        @"retainedSetter": ^(CGRect newRect) {
-            NSLog(@"Try to modify by Lookin.");
-        }
-    }];
-    
-    // size property
-    [properties addObject:@{
-        @"section": @"Animal Info",
-        @"title": @"CatSize",
-        @"value": [NSValue valueWithCGSize:CGSizeMake(5, 6)],
-        @"valueType": @"size",
-        @"retainedSetter": ^(CGSize newSize) {
-            NSLog(@"Try to modify by Lookin.");
-        }
-    }];
-    
-    // point property
-    [properties addObject:@{
-        @"section": @"Animal Info",
-        @"title": @"CatPoint",
-        @"value": [NSValue valueWithCGPoint:CGPointMake(7, 8)],
-        @"valueType": @"point",
-        @"retainedSetter": ^(CGPoint newPoint) {
-            NSLog(@"Try to modify by Lookin.");
-        }
-    }];
-    
-    // insets property
-    [properties addObject:@{
-        @"section": @"Animal Info",
-        @"title": @"CatInsets",
-        @"value": [NSValue valueWithUIEdgeInsets:UIEdgeInsetsMake(1, 2, 3, 4)],
-        @"valueType": @"insets",
-        @"retainedSetter": ^(UIEdgeInsets newInsets) {
-            NSLog(@"Try to modify by Lookin.");
-        }
-    }];
-    
-    // shadow property
-    [properties addObject:@{
-        @"section": @"Animal Info",
-        @"title": @"CatShadow",
-        @"value": @{
-            @"opacity": @0.5,
-            @"offset": [NSValue valueWithCGSize:CGSizeMake(5, 10)],
-            @"radius": @2.5,
-            // 可选项，没有该项则表示颜色为 nil
-            // Optional. Do not set this item when the shadow color is nil.
-            @"color": UIColor.redColor
-        },
-        @"valueType": @"shadow"
-    }];
-    
-    // enum property
-    [properties addObject:@{
-        @"section": @"Animal Info",
-        @"title": @"Type",
-        @"value": @"Corgi",
-        @"valueType": @"enum",
-        // 当 valueType 为 enum 时，必须设置该项，内容为所有可用的 enum 值。
-        // When valueType is "enum", this item must be set, with the content being all available enum cases.
-        @"allEnumCases": @[@"Corgi", @"Samoyed", @"Golden Retriever", @"Teddy"],
-        @"retainedSetter": ^(NSString *newValue) {
-            NSLog(@"Try to modify by Lookin.");
-        }
-    }];
-    
-    // json property
-    [properties addObject:@{
-        @"section": @"Animal Info",
-        @"title": @"CatJson",
-        @"value": [self createSomeJson],
-        @"valueType": @"json"
     }];
     
     return [properties copy];
@@ -225,6 +100,8 @@
 
     [subviews addObject:@{
         @"title": @"SomeConfig",
+        // 可选项。为 DanceUI 项目组预留的字段，项目组之外的人请忽略
+        // Optional. Reserved fields for the DanceUI project team, please ignore if you are not part of the team.
         @"lookin_source": [self createDanceUIJson],
         // 可选项。你可以递归地添加你的虚拟 subview。
         // Optional. You can recursively add your virtual subview.
@@ -235,39 +112,6 @@
     }];
     
     return [subviews copy];
-}
-
-- (NSString *)createSomeJson {
-    NSArray *arr = @[
-        @{
-            @"title":  @"allowedBehaviors",
-            @"desc": @"HostingControllerAllowedBehaviors",
-            @"details": @[
-                @{
-                    @"title":  @"rawValue",
-                    @"desc": @"0"
-                }, @{
-                    @"title":  @"contentScrollViewBridge",
-                    @"desc": @"UIKitContentScrollViewBridge",
-                    @"details": @[
-                        @{
-                            @"title":  @"bridgeSetEdges",
-                            @"desc": @"[:]"
-                        }, @{
-                            @"title":  @"pixelLength",
-                            @"desc": @"0.33333333"
-                        }]
-                }]
-        },
-        @{
-            @"title":  @"scenePhase",
-            @"desc": @"active"
-        }
-    ];
-    NSError *error;
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:arr options:0 error:&error];
-    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    return jsonString;
 }
 
 - (NSString *)createDanceUIJson {
